a70487b8d898fa194b7c78fd6ef18cb416a56933
Joval Gordon <jgordon95@hotmail.co.uk>
2025-02-22 20:00:21 +0000

    half baked

---
+++ BlazorApp1/OpenAIServices/OpenAIServices_ChatClient.cs
@@ -0,0 +1,26 @@
+﻿using OpenAI.Chat;
+
+namespace BlazorApp1.OpenAIServices
+{
+    public partial class OpenAIServices
+    {
+        public ChatClient OpenAIClientService(IConfiguration _Configuration)
+        {
+            ChatClient? chatClientService = null;
+
+            try
+            {
+                string chatModel = _Configuration["OpenAI:Model"];
+                string chatAPIKey = _Configuration["OpenAI:APIKey"];
+
+                chatClientService = new(model: chatModel, apiKey: chatAPIKey);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("OpenAIClientService Ex: " + ex.Message);
+            }
+
+            return chatClientService;
+        }
+    }
+}
