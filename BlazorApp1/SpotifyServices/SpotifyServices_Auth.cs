fe1603d0573687a8b92e1018f3765bb0724f7c29
Joval Gordon <jgordon95@hotmail.co.uk>
2025-02-22 23:31:39 +0000

    Session storage fix

--- BlazorApp1/SpotifyServices/SpotifyServices_Auth.cs
+++ BlazorApp1/SpotifyServices/SpotifyServices_Auth.cs
@@ -3,6 +3,8 @@ using static System.Net.WebRequestMethods;
 using BlazorApp1.Data;
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorApp1.SpotifyServices
 {
@@ -16,9 +18,12 @@ namespace BlazorApp1.SpotifyServices
 
         private readonly IJSRuntime _jsRuntime;
 
-        public SpotifyAppServices(IConfiguration configuration)
+        public ProtectedSessionStorage _ProtectedSessionStorage;
+
+        public SpotifyAppServices(IConfiguration configuration, ProtectedSessionStorage protectedSessionStorage)
         {
             _Configuration = configuration;
+            _ProtectedSessionStorage = protectedSessionStorage;
 
             clientId = _Configuration["SpotifyWeb:ClientId"];
             clientSecret = _Configuration["SpotifyWeb:ClientSecret"]; ;
@@ -35,7 +40,7 @@ namespace BlazorApp1.SpotifyServices
             {
                 string scopes = _Configuration["SpotifyWeb:Scopes"];
 
-                spotifyAuthUrl = $"{spotifyAuthAddress}?client_id={clientId}&response_type=code&redirect_uri={Uri.EscapeDataString(nUrl)}&scopes={Uri.EscapeDataString(scopes)}";
+                spotifyAuthUrl = $"{spotifyAuthAddress}?client_id={clientId}&response_type=code&redirect_uri={Uri.EscapeDataString(redirectUri)}&scopes={Uri.EscapeDataString(scopes)}";
             }
             catch (Exception ex)
             {
@@ -45,33 +50,46 @@ namespace BlazorApp1.SpotifyServices
             return spotifyAuthUrl;
         }
 
-        //public async Task<SpotifyTokenResponse> ExchangeCodeForToken(string code)
-        //{
-        //    HttpClient httpClient = new HttpClient();
+        public async Task ExchangeCodeForToken(string code)
+        {
+            HttpClient httpClient = new HttpClient();
 
-        //    var requestContent = new FormUrlEncodedContent(new[]
-        //    {
-        //    new KeyValuePair<string, string>("grant_type", "authorization_code"),
-        //    new KeyValuePair<string, string>("code", code),
-        //    new KeyValuePair<string, string>("redirect_uri", redirectUri),
-        //    new KeyValuePair<string, string>("client_id", clientId),
-        //    new KeyValuePair<string, string>("client_secret", clientSecret),
-        //});
+            var requestContent = new FormUrlEncodedContent(new[]
+            {
+            new KeyValuePair<string, string>("grant_type", "authorization_code"),
+            new KeyValuePair<string, string>("code", code),
+            new KeyValuePair<string, string>("redirect_uri", redirectUri),
+            new KeyValuePair<string, string>("client_id", clientId),
+            new KeyValuePair<string, string>("client_secret", clientSecret),
+        });
 
-        //    var response = await httpClient.PostAsync("https://accounts.spotify.com/api/token", requestContent);
-        //    var responseContent = await response.Content.ReadFromJsonAsync<SpotifyTokenResponse>();
+            var response = await httpClient.PostAsync("https://accounts.spotify.com/api/token", requestContent);
+            var responseContent = await response.Content.ReadFromJsonAsync<SpotifyTokenResponse>();
 
-        //    if (responseContent != null)
-        //    {
+            if (responseContent != null)
+            {
+                //ProtectedSessionStorage protectedSessionStorage = new ProtectedSessionStorage();
+                _ProtectedSessionStorage.SetAsync("spotify_token", responseContent.access_token);
+                Console.WriteLine("ExchangeCodeForToken accessToken: " + responseContent.access_token);
+                // Store the token securely (e.g., local storage or session)
+                //*/*/*/*/*/*/Navigation.NavigateTo("/");*/*/*/*/*/*/
+                //return responseContent;
+            }
+            //else
+            //{
+            //    return null;
+            //}
+        }
 
-        //        // Store the token securely (e.g., local storage or session)
-        //        //*/*/*/*/*/*/Navigation.NavigateTo("/");*/*/*/*/*/*/
-        //        return responseContent;
-        //    }
-        //    else
-        //    {
-        //        return null;
-        //    }
-        //}
-    }
+        public async Task<string> GetTokenAsync()
+        {
+            var result = await _ProtectedSessionStorage.GetAsync<string>("spotify_token");
+            if (result.Success)
+            {
+                Console.WriteLine($"Stored Value: {result.Value}");
+                return result.Value;
+            }
+            return "";
+        }
+}
 }
