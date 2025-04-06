fce3b5fe9d50ed097724dfd427a55ca58399ee9b
Joval Gordon <jgordon95@hotmail.co.uk>
2025-04-06 18:09:23 +0100

    Getting Playlists

--- BlazorApp1/wwwroot/javascript/spotify.js
+++ BlazorApp1/wwwroot/javascript/spotify.js
@@ -1,1 +1,25 @@
-﻿
+﻿function spotifyDisplayPlaylists(jsonListPlaylists) {
+    console.debug("spotifyDisplayPlaylists: START");
+    console.debug("spotifyDisplayPlaylists jsonlistPlaylists: " + jsonListPlaylists);
+
+    let obj = JSON.parse(jsonListPlaylists);
+
+    let elm_img;
+
+    const spotifyPlaylistsDisplay = document.getElementById("spotifyPlaylistsDisplay");
+
+    
+
+    for (index in obj) {
+
+        elm_img = document.createElement("img");
+        elm_img.classList.add("w3-bar-item");
+        if (obj[index].Images != null) {
+            elm_img.src = obj[index].Images[0].Url;
+        }
+        elm_img.style.width = "200px"
+        elm_img.style.height = "200px"
+        spotifyPlaylistsDisplay.appendChild(elm_img);
+        
+    }
+}
