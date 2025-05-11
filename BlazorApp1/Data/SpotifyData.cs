a2cce7824571b99dd73768dff659c7040ca0eaea
Joval Gordon <jgordon95@hotmail.co.uk>
2025-05-11 13:50:49 +0100

    Altered structure

--- BlazorApp1/Data/SpotifyData.cs
+++ BlazorApp1/Data/SpotifyData.cs
@@ -7,10 +7,63 @@
         public int expires_in { get; set; }
         public string refresh_token { get; set; }
     }
-
-    public class SpotifyUser
+    public class SpotifyAuthUser
     {
         public string DisplayName { get; set; }
-        public string Email { get; set; }
+        public string SpotifyID { get; set; }
+    }
+    public class SpotifyPlaylists
+    {
+        public string Next { get; set; }
+        public int Total { get; set; }
+        public List<SpotifyPlaylist> Items = new List<SpotifyPlaylist>();
+    }
+    public class SpotifyPlaylist
+    {
+        public string Description { get; set; }
+        public string Href { get; set; }
+        public string Id { get; set; }
+        public List<PlaylistImage> Images { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public PlaylistOwner Owner { get; set; }
+        public PlaylistTracks Tracks { get; set; }
+    }
+    public class PlaylistImage
+    {
+        public string Url { get; set; }
+        public int? Height { get; set; }
+        public int? Width { get; set; }
+    }
+    public class PlaylistOwner
+    {
+        public string DisplayName { get; set; }
+    }
+    public class PlaylistTracks
+    {
+        public string Next { get; set; }
+        public int Total { get; set; }
+        public List<TrackItem> Items = new List<TrackItem>();
+    }
+    public class TrackItem
+    {
+        public Track Track = new Track();
+    }
+    public class Track
+    {
+        public Album Album { get; set; }
+        public List<Artists> Artists { get; set; }
+        public int Duration_ms { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string TrackTime { get; set; }
+    }
+    public class Album
+    {
+        public string Name { get; set; }
+    }
+    public class Artists
+    {
+        public string Name { get; set; }
     }
 }
