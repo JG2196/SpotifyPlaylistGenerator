b03e5bc9d1afcaa6b126e0dd245157906719026e
Joval Gordon <jgordon95@hotmail.co.uk>
2025-05-23 21:51:44 +0100

    Successful Playlist Creation

--- BlazorApp1/wwwroot/javascript/spotify.js
+++ BlazorApp1/wwwroot/javascript/spotify.js
@@ -94,6 +94,8 @@ function spotifyProcessChatResponse(jsonData) {
         var elm_Li = document.createElement("li");
 
         elm_Li.setAttribute("spotify_id", obj.playlist[index].spotify_id);
+        elm_Li.setAttribute("track_name", obj.playlist[index].title);
+        elm_Li.setAttribute("track_artist", obj.playlist[index].artist);
         elm_Li.style.listStyleType = "none";
 
         elm_Li.innerText = `${obj.playlist[index].title} - ${obj.playlist[index].artist}`;
@@ -115,17 +117,24 @@ function showLoading() {
 
 function getTracks() {
 
-    var listTrackIds = [];
+    var listTracks = [];
+    //var listTrackIds = [];
 
     var aiListPlaylistTracks = document.getElementById("ai_ListPlaylistTracks");
     var child_Li = aiListPlaylistTracks.getElementsByTagName("li");
 
     for (var i = 0; i < child_Li.length; i++) {
-        var spotifyTrackId = child_Li[i].getAttribute("spotify_id");
-        listTrackIds.push(spotifyTrackId);
-    }
+        var spotifyTrackName = child_Li[i].getAttribute("track_name");
+        var spotifyTrackArtist = child_Li[i].getAttribute("track_artist");
 
-    return listTrackIds;
+        var CreateTrack = {
+            artist: spotifyTrackArtist,
+            name: spotifyTrackName
+        }
+        listTracks.push(CreateTrack);
+    }
+    //JSON.stringify(listTracks)
+    return JSON.stringify(listTracks);
 }
 
 function tryMe() {
