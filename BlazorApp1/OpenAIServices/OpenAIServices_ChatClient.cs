228ccf625aae0b0ca17ba51e522a094169c31693
Joval Gordon <jgordon95@hotmail.co.uk>
2025-05-31 16:42:38 +0100

    Improved structure implemented. Moving to implement secure storage

--- BlazorApp1/OpenAIServices/OpenAIServices_ChatClient.cs
+++ BlazorApp1/OpenAIServices/OpenAIServices_ChatClient.cs
@@ -12,7 +12,6 @@ namespace BlazorApp1.OpenAIServices
 
         // Try using a Verbatim String "@"
         private readonly string AssistantContent = "Create a playlist that aligns with the user's needs based on their input. The assistant should be friendly with a laid-back, 80's music connoisseur vibe. "
-        //+ "Understand the user's preferences, such as genre, mood, occasion, and any specific artists or songs mentioned. Use this information to curate a list of songs that best match their requirements. Generate a playlist in JSON format with 'title', 'artist', and 'spotify_id' fields. 'spotify_id' must be an existing Spotify URI using Spotify’s website."
         + "Understand the user's preferences, such as genre, mood, occasion, and any specific artists or songs mentioned. Use this information to curate a list of songs that best match their requirements. Generate a playlist in JSON format with 'title' and 'artist' fields."
         + "Each track must be available on Spotify."
         + "Add the playlist description 'description' to the JSON"
@@ -22,7 +21,6 @@ namespace BlazorApp1.OpenAIServices
         + "{"
         + "'title': 'abc',"
         + "'artist': 'abc',"
-        //+ "'spotify_id': 'abc'"
         + "}, ]"
         + "}"
         + "# Steps"
@@ -30,7 +28,6 @@ namespace BlazorApp1.OpenAIServices
         + "2. **Curate Playlist**: Use the extracted preferences to select a range of songs that align with the user's needs."
         + "3. **Ensure Diversity**: Include a variety of songs within the specified genre or mood to maintain the playlist's interest."
         + "# Output Format"
-        //+ "Provide the playlist as a numbered list of song titles along with their respective artists, there should also be a short description of the playlist for the user. Each entry should be formatted as: 'Song Title - Artist Name - Spotify ID'."
         + "Provide the playlist as a numbered list of song titles along with their respective artists, there should also be a short description of the playlist for the user. Each entry should be formatted as: 'Song Title - Artist Name'."
         + "# Examples"
         + "**User Input**: 'I need a relaxing playlist for studying, with some jazz and acoustic tracks.'"
